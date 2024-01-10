398b262008624099f9dc6da62f3d383faf9269e8
mathewa <awamathew4@gmail.com>
2024-01-10 12:58:51 -0500

    print my first name / go liverpool

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #TACE Final culminating
 #This game is liverpool jeopardy
 
+#TACE Final culminating
+#This game is liverpool jeopardy
+
 import pygame
 import sys
 
 Boxinfo2 = [[20, 330, "CP1.png"]]
-Boxinfo = [[20, 350, "CP1.png"], [20,450,"CP2.png"],[20,550,"CP3.png"],[20,650,"CP4.png"],[20,750,"CP5.png"]]
-          # [20,10,"CP1.png"],[20,20,"CP2.png"],[20,30,"CP3.png"],[20,40,"CP4.png"],[20,50,"CP5.png"]]
+Boxinfo = [[20, 300, "CP1.png"], [20,450,"CP2.png"],[20,580,"CP3.png"],[20,700,"CP4.png"],[20,820,"CP5.png"],
+[300,300,"CP1.png"],[300,450,"CP2.png"],[300,580,"CP3.png"],[300,700,"CP4.png"],[300,820,"CP5.png"],
+[600,300,"CP1.png"],[600,450,"CP2.png"],[600,580,"CP3.png"],[600,700,"CP4.png"],[600,820,"CP5.png"],
+[870,300,"CP1.png"],[870,450,"CP2.png"],[870,580,"CP3.png"],[870,700,"CP4.png"],[870,820,"CP5.png"]]
 
 class BOX:
     def __init__(self, x,y,filename):
@@ -17,6 +22,8 @@ class BOX:
         self.image = pygame.transform.scale(self.image, self.size)
         self.rect = self.image.get_rect()
         self.rect = self.rect.move(self.x, self.y)
+
+
 
 #main
 win = pygame.display.set_mode((1100, 900))
@@ -39,6 +46,9 @@ for i in range(len(Boxinfo)):
 
 while True:
 
+    mouse_pos=pygame.mouse.get_pos
+    mouseRect=pygame.draw.rect(Window,white,[mouse_pos[0],mouse_[1],rect_size, rect_size])
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -50,3 +60,17 @@ while True:
         win.blit(Board[i].image, Board[i].rect)
  #   win.blit(Box1.image, Box1.rect)
     pygame.display.update()
+
+ def checkCollision(self, mouseRect):
+    col=self.rect.colliderect(sprite1.rect)
+    if col == True:
+        files= input()
+
+        file = open(files, "r")
+        text = file.read()
+
+        for line in text:
+            for letter in line:
+                print(letter)
+
+        file.close()
